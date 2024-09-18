d5410b87b49d068aea086e479d4c8e428ce7aa51
abumusa8 <abumusa0740@gmail.com>
2024-09-18 23:36:50 +0600

    first commit

---
+++ js/hellow.js
@@ -0,0 +1,6 @@
+document.getElementById('login-btn').addEventListener('click', function(){
+    const passwor =document.getElementById('password').value
+    if(passwor === 222){
+       
+    }
+})
