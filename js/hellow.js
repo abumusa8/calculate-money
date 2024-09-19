f0e589fd73847120e0b4a43e2f14de9a930b3324
abumusa8 <abumusa0740@gmail.com>
2024-09-19 11:45:49 +0600

    add some requre

--- js/hellow.js
+++ js/hellow.js
@@ -1,6 +1,17 @@
-document.getElementById('login-btn').addEventListener('click', function(){
-    const passwor =document.getElementById('password').value
-    if(passwor === 222){
-       
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault()// loding problem solvd
+    const phonenumber =document.getElementById('phone-number').value;
+    const pinnumber=document.getElementById('pin-number').value;
+
+    if(phonenumber === '1' && pinnumber === '1'){
+        window.location.href='/home.html'
+      
+
     }
+
+    else{
+        alert('invalid input')
+    }
+   console.log(phonenumber)
+   console.log(pinnumber)
 })
---
+++ js/home.js
@@ -0,0 +1,21 @@
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault()
+    const inputmoney = document.getElementById('input-money').value;
+    const inputpinnumber = document.getElementById('input-pin-number').value;
+     
+
+    if(inputpinnumber === '123'){
+    console.log('login confrom')
+        const blance = document.getElementById('blance').innerText
+        const addmoneynumber =parseFloat(inputmoney)
+        const addomneyblace =parseFloat(blance)
+        
+        const newblance =addmoneynumber+addomneyblace
+        document.getElementById('blance').innerText =newblance
+
+    }
+else{
+    alert('faild to add money..!')
+}
+
+})
